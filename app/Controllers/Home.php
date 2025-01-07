d01b4b4afdfd369134ca082fced5d3d07c82b89c
zhalaudien <zhalaudien@gmail.com>
2025-01-07 11:50:47 +0700

    menambhakan tema adminlte4

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,11 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        echo view("pages/header");
+        echo view("pages/navbar");
+        echo view("index");
+        echo view("pages/footer");
     }
 }
